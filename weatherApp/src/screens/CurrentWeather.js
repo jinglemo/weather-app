f7209bbf0e72e852a218164b031a55105c5668df
Jing Mo <Jingmo39@gmail.com>
2024-01-11 22:16:33 -0500

    added tabs

--- weatherApp/src/screens/CurrentWeather.js
+++ weatherApp/src/screens/CurrentWeather.js
@@ -1,35 +1,44 @@
 import React from 'react'
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import RowText from '../components/RowText'
 
 const CurrentWeather = () => {
+  const { wrapper, container, temp, feels, highLow, highLowWrapper, bodyWrapper, description, message } = styles
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
+    <SafeAreaView style={wrapper}>
+      <View style={container}>
         <Feather name="sun" size={50} color="black" />
-        <Text style={styles.temp}>21</Text>
-        <Text style={styles.feels}>feels 16</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>high 25 </Text>
-          <Text style={styles.highLow}>low 10</Text>
-        </View>
+        <Text style={temp}>21</Text>
+        <Text style={feels}>feels 16</Text>
+        <RowText
+          messageOne={'high: 81'}
+          messageTwo={'low: 67'}
+          containerStyles={highLowWrapper}
+          messageOneStyles={highLow}
+          messageTwoStyles={highLow}
+        />
       </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>Sunny</Text>
-        <Text style={styles.message}>Its perfect t-shirt weather</Text>
-      </View>
+      <RowText
+        messageOne={'Its Sunny'}
+        messageTwo={'Its perfect t-shirt weather'}
+        messageOneStyles={description}
+        messageTwoStyles={message}
+        containerStyles={bodyWrapper}
+      />
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1
+    flex: 1,
+    backgroundColor: 'pink'
   },
   container: {
     backgroundColor: 'pink',
     flex: 2,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   temp: {
     color: 'black',
@@ -50,7 +59,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'flex-start',
     paddingLeft: 25,
-    marginBottom: 40,
+    marginBottom: 40
   },
   description: {
     fontSize: 38
